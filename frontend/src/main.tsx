4d3f97a7a9ea5e495bd3f69bfe809e976a412537
Shradha pateriya <pateriyashraddha78@gmail.com>
2026-09-22 23:44:00 +0530

    feat: add React application entry

---
+++ frontend/src/main.tsx
@@ -0,0 +1,2 @@
+import React from "react";import{createRoot}from"react-dom/client";import{QueryClient,QueryClientProvider}from"@tanstack/react-query";import{BrowserRouter}from"react-router-dom";import{Toaster}from"sonner";import"./index.css";import App from "./App";
+const qc=new QueryClient({defaultOptions:{queries:{staleTime:60000,retry:1}}});createRoot(document.getElementById("root")!).render(<React.StrictMode><QueryClientProvider client={qc}><BrowserRouter><App/></BrowserRouter><Toaster richColors position="bottom-right"/></QueryClientProvider></React.StrictMode>);
